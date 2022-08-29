3c5adf63feceb47535a801125f6e65166f3b2e96
unknown <randrew934@gmail.com>
2022-08-29 06:17:46 +0100

    29082022 ReCaptcha Secret Key Updated

--- pages/api/enquiry.js
+++ pages/api/enquiry.js
@@ -1,5 +1,6 @@
 const handler = async (req, res) => {
     const baseUrl = "https://yonetwork.org";
+    const secretKey = "6LdwpLUhAAAAAHCU1s8ETTIrE__Vz1yB1PegElSd"
 
     if (req.method === "POST") {
         try {
@@ -8,7 +9,7 @@ const handler = async (req, res) => {
                 headers: {
                     "Content-Type": "application/x-www-form-urlencoded",
                 },
-                body: `secret=6LdwpLUhAAAAAHCU1s8ETTIrE__Vz1yB1PegElSd&response=${req.body.gRecaptchaToken}`,
+                body: `secret=${secretKey}&response=${req.body.gRecaptchaToken}`,
             });
             let reCaptchaRes = await reCaptchaResponse.json();
             if (reCaptchaRes?.score > 0.5) {
