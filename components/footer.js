22d01850dcc3176d82e408d9fb6e67dcc274c683
unknown <randrew934@gmail.com>
2022-09-06 19:58:06 +0100

    06092022 Google Analytics and UI update

--- components/footer.js
+++ components/footer.js
@@ -1,32 +1,33 @@
-import Script from 'next/script'
+import Script from 'next/script';
+import Link from 'next/link';
 
 function Footer() {
     return (
         <div className="container-fluid mission-box">
             <div className="container">
                 <div className="row p-5 ">
-                    <div className="col-lg-4">
+                    <div className="col-lg-4 mb-4">
                         <img
                             src="/assets/images/footer-logo-mega-fi.png"
                             className="Image-fluid"
                             alt="MegaFi"
                         />
-                        <div className="row mt-5">
+                        <div className="row mt-3 mt-md-5">
                             <p>
                                 Internet Of NFTs.{" "}
                             </p>
                         </div>
                     </div>
-                    <div className="col-lg-2">
+                    <div className="col-lg-2 mb-4">
                         <h5>Quick Links</h5>
                         {/* <p>
                             <a href="#"> Docs</a>
                         </p> */}
-                        <p>
+                        <p className="mt-4">
                             <a href="https://megafiprotocol.github.io/docs/pages/contact/" target="_blank" rel="noopener noreferrer"> Contact</a>
                         </p>
                     </div>
-                    <div className="col-lg-2">
+                    <div className="col-lg-2 mb-4">
                         <h5>Community</h5>
                         <p>
                             {/* <a href="#"> Help Center</a> */}
@@ -34,26 +35,30 @@ function Footer() {
                         <p>
                             {/* <a href="#"> Careers</a> */}
                         </p>
-                        <p>
+                        <p className="mt-4">
                             <a href="https://medium.com/@megafiprotocol" target="_blank" rel="noopener noreferrer">  Blog</a>
                         </p>
                     </div>
-                    <div className="col-lg-4">
+                    <div className="col-lg-4 mb-4">
                         <h5>Follow us on</h5>
-                        <div className="row">
+                        <div className="row mt-2">
                             <div className="col-lg-4">
-                                <p className="icon p-2">
-                                    <a href="https://t.me/+LdFqD-mMu082ZjYx" target="_blank" rel="noopener noreferrer">
-                                        <i className="fab fa-telegram-plane" />
-                                    </a>
-                                </p>
+                                <div className="icon p-2 mt-3">
+                                    <Link href="https://t.me/+LdFqD-mMu082ZjYx/">
+                                        <a className="nav-link" target="_blank" rel="noopener noreferrer">
+                                            <i className="fab fa-telegram-plane" />
+                                        </a>
+                                    </Link>
+                                </div>
                             </div>
                             <div className="col-lg-4">
-                                <p className="icon p-2">
-                                    <a  href="https://twitter.com/megafiprotocol" target="_blank" rel="noopener noreferrer">
-                                        <i className="fab fa-twitter" />
-                                    </a>
-                                </p>
+                                <div className="icon p-2 mt-3">
+                                    <Link href="https://twitter.com/megafiprotocol/">
+                                        <a className="nav-link" target="_blank" rel="noopener noreferrer">
+                                            <i className="fab fa-twitter" />
+                                        </a>
+                                    </Link>
+                                </div>
                             </div>
                             {/* <div className="col-lg-4">
                                 <p className="icon p-2">
@@ -72,11 +77,13 @@ function Footer() {
                                 </p>
                             </div> */}
                             <div className="col-lg-4">
-                                <p className="icon p-2">
-                                    <a href="https://www.tiktok.com/t/ZTdn66sdn/" target="_blank" rel="noopener noreferrer">
-                                        <i className="fab fa-tiktok" />
-                                    </a>
-                                </p>
+                                <div className="icon p-2 mt-3">
+                                    <Link href="https://www.tiktok.com/t/ZTdn66sdn/">
+                                        <a className="nav-link" target="_blank" rel="noopener noreferrer">
+                                            <i className="fab fa-tiktok" />
+                                        </a>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
                     </div>
